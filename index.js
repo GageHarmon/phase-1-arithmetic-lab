485e3d42a351f54dbf0206cd0f27ca82ce8b91f9
Gage Harmon <gaharmon1@gmail.com>
2023-01-30 17:15:46 -0700

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() +1);
+
+
+let num3 = 48
+let num4 = 11
+let mod = num3 % num4;
+
+let max = Math.max(2, 3, 20);
